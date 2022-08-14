a2f18fb624577d43f6ce6458bdb49141d164f0f8
zaynab88 <ahmedzaynab204@gmail.com>
2022-08-14 09:42:25 +0200

    gg

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,23 +1,25 @@
 import jwt from 'jsonwebtoken'
 import { AuthRequest, MyToken } from '../types'
 
-export const isAuthenticated = async (req: AuthRequest, res, next) => {    const { authorization } = req.headers
-    if(!authorization){
-        return res.status(403).send(" A token is requried for authentication");
-    
-    }
-    jwt.verify(
-      authorization,
-      process.env.TOKEN_KEY!,
-      async (err, token: MyToken) => {
-        if (err) {
-          return res.status(401).json({
-            message: 'Invalid token',
-          })
-        }
-        req.email = token.email
-        next()
-  
+export const isAuthenticated = async (req: AuthRequest, res, next) => {
+  const { authorization } = req.headers
+  if (!authorization) {
+    return res.status(403).send(" A token is requried for authentication");
+
+  }else{
+  jwt.verify(
+    authorization,
+    process.env.TOKEN_KEY!,
+    async (err, token: MyToken) => {
+      if (err) {
+        return res.status(401).json({
+          message: 'Invalid token',
+        })
       }
-    )
-  }
+      req.email = token.email
+      next()
+   
+    }
+  )
+}
+}
